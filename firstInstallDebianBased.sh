6f3817ca6eccfe5b7dd917d6a937658e4328aa68
Don Fanning <don@00100100.net>
2020-04-08 15:08:57 -0700

    Update firstInstallDebianBased.sh

--- firstInstallDebianBased.sh
+++ firstInstallDebianBased.sh
@@ -118,7 +118,6 @@ aptitude install \
   ttf-mscorefonts-installer \
   ttf-bitstream-vera \
   ttf-dejavu \
-  ttf-liberation \
   ttf-linux-libertine \
   ttf-larabie-deco \
   ttf-larabie-straight \
